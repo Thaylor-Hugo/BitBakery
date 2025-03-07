7b32442ea462c40116752b097e98f3ca1a99e3a5
Thaylor-Hugo <thaylorhugo@gmail.com>
2025-03-06 21:11:24 -0300

    💄 Basic style for home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,36 @@
 import { useGameChooser } from '../hooks/useGameChooser';
 
 function Header({ title }) {
-    return <h1>{title ? title : "Default title"}</h1>
+    return <h1 class="text-9xl text-center">{title ? title : "Default title"}</h1>
+}
+
+// Component for showing a minigame icon
+function House({ game }) {
+    let src = ""
+    if (game === "cakegame") {
+        src = "https://gallery.yopriceville.com/var/resizes/Free-Clipart-Pictures/Cakes-PNG/Cake_PNG_Transparent_Clip_Art_Image.png?m=1629830081"
+    } else if (game === "chothesgame") {
+        src = "https://www.pngplay.com/wp-content/uploads/6/Clothes-Vector-Background-PNG-Image.png"
+    } else if (game === "memorygame") {
+        src = "https://pngimg.com/uploads/brain/brain_PNG15.png"
+    }
+    return<div class="flex flex-col items-center justify-center">
+            <img class="w-1/2" src={src} />
+            <p class="text-3xl text-center">{game}</p>
+        </div>
+    
 }
 
 export default function HomePage() {
     useGameChooser();
     return (
         <div>
-            <Header title="Develop. Preview. Ship." />
-            <p>Hello World</p>
+            <Header title="BitBakery" />
+            <div class="columns-3">
+                <House game="cakegame" />
+                <House game="chothesgame" />
+                <House game="memorygame" />
+            </div>
         </div>
     )
 }
