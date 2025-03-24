8e28395284dba6e2cdb35b7a60b1a3b6a0313dc2
Thaylor-Hugo <thaylorhugo@gmail.com>
2025-03-24 15:16:13 -0300

    🚨 Fix analog errors

--- api/analog_service.py
+++ api/analog_service.py
@@ -9,7 +9,7 @@ sensors = {
 }
 
 cake_states = ["inicio", "preparation", "show_play", "show_interval", "next_show", "initiate_play", "wait_play", 
-                "register_play", "compare_play", "next_play", "start_show", "register_show", "end_state"]
+                "register_play", "compare_play", "next_play", "start_show", "register_show", "end_state", "Erro", "Erro", "Erro"]
 
 genius_states =["inicial", "preparacao", "proxima_mostra", "espera_jogada", "registra_jogada", "compara_jogada", 
                 "proxima_jogada", "foi_ultima_sequencia", "proxima_sequencia", "mostra_jogada", "intervalo_mostra", 
@@ -17,8 +17,6 @@ genius_states =["inicial", "preparacao", "proxima_mostra", "espera_jogada", "reg
 
 minigames = ["memorygame", "cakegame", "clothesgame"]
 device_name = "Analog Discovery 2"
-device_data = None
-supplies_data = None
 
 
 def convert_dec(bin):
@@ -29,52 +27,38 @@ def convert_dec(bin):
     return dec
 
 
-def init_analog():
+def analog_loop():
     # connect to the device
+    global device_data
     device_data = device.open()
     device_data.name = device_name
-    
+        
     # start the positive supply
+    global supplies_data 
     supplies_data = supplies.data()
     supplies_data.master_state = True
     supplies_data.state = True
     supplies_data.voltage = 3.3
     supplies.switch(device_data, supplies_data)
-
-def set_output(index, state):
-    static.set_state(device_data, index, state)
-
-def analog_loop():
-    init_analog()
-
     # set all pins as input
     for index in range(16):
-        set_output(index, False)
-    
-    try:
-        while True:
-            # go through possible states
-            sensors_temp = []
-            for index in range(16):
-                # set the state of every DIO channel
-                sensors_temp.append(static.get_state(device_data, index))
-            sleep(0.001)  # delay
-            sensors["jogada"] = sensors_temp[:7]
-            sensors["minigame"] = minigames[convert_dec(sensors_temp[11:14])]
-            if (sensors["minigame"] == "cakegame"):
-                sensors["state"] = cake_states[convert_dec(sensors_temp[7:11])]
-            else:
-                sensors["state"] = genius_states[convert_dec(sensors_temp[7:11])]
-            sensors["pontuacao"] = convert_dec(sensors_temp[14:])
-            
-            print(sensors)
-
-    except KeyboardInterrupt:
-        # stop if Ctrl+C is pressed
-        pass
-    finally:
-        close_analog()
-    
+        static.set_mode(device_data, index, False)
+    while True:
+        # go through possible states
+        sensors_temp = []
+        for index in range(16):
+            # set the state of every DIO channel
+            sensors_temp.append(static.get_state(device_data, index))
+        # sleep(0.0001)  # delay
+        sensors["jogada"] = sensors_temp[:7]
+        sensors["minigame"] = minigames[convert_dec(sensors_temp[11:13])]
+        if (sensors["minigame"] == "cakegame"):
+            sensors["state"] = cake_states[convert_dec(sensors_temp[7:11])]
+        else:
+            sensors["state"] = genius_states[convert_dec(sensors_temp[7:11])]
+        sensors["pontuacao"] = convert_dec(sensors_temp[13:])
+        print(sensors)
+   
 
 def close_analog():
     # stop the static I/O
