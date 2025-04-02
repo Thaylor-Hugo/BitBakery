23ae79cf2913369f32d8766c3f606cbd3e926380
BrielGa01 <gabrielldasilva969@usp.br>
2025-04-02 11:35:28 -0300

    🐛 Fix state variable types

--- src/hooks/memorygame.js
+++ src/hooks/memorygame.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
-export function useMemoryGame() { 
+export function useMemoryGame() {
     const router = useRouter();
     const [jogada, setJogada] = useState();
     const [pontuacao, setPontuacao] = useState(0);
     const [gameOver, setGameOver] = useState(false);
+    const [estadoMudou, setEstadoMudou] = useState(false);
     const estados_finais = ["final_timeout", "final_acertou", "final_errou"];
     const estados_mostra = ["proxima_mostra", "mostra_jogada", "intervalo_mostra"];
 
@@ -21,11 +22,10 @@ export function useMemoryGame() {
                 } else if (sensors.state == "preparacao") {
                     setPontuacao(0);
                     setGameOver(false);
-                }
-                else if (sensors.state == "espera_jogada") {
-                    estado_mudou = true;
-                } else if (estados_mostra.includes(sensors.state) && estado_mudou) {
-                    estado_mudou = false;
+                } else if (sensors.state == "espera_jogada") {
+                    setEstadoMudou(true);
+                } else if (estados_mostra.includes(sensors.state) && estadoMudou) {
+                    setEstadoMudou(false);
                     setPontuacao(prevpontuacao => prevpontuacao + 1);
                 } else if (estados_finais.includes(sensors.state)) {
                     setPontuacao(prevpontuacao => prevpontuacao + (sensors.state == "final_acertou" ? 1 : 0));
@@ -35,8 +35,8 @@ export function useMemoryGame() {
                 console.error('Error fetching sensors:', error);
             }
         };
-        const interval = setInterval(fetchSensors, 50);
+        const interval = setInterval(fetchSensors, 25);
         return () => clearInterval(interval);
-    }, [router]);
+    }, [router, estadoMudou, gameOver]);
     return { jogada, pontuacao, gameOver };
 }
