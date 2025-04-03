04dacb48e7352e07c11ed2baa2a2affcb4be1b81
Thaylor-Hugo <thaylorhugo@gmail.com>
2025-04-03 20:26:33 -0300

    Style gameover

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,7 +1,7 @@
 import Header from './basic';
 
 export default function GameOver({ pontuacao }) {
-    const title = "Game Over!";
+    const title = "Fim de Jogo!";
     const messagePontuacao = `Sua pontuação foi: ${pontuacao} pontos de 16.`;
     
     let message;
@@ -15,17 +15,17 @@ export default function GameOver({ pontuacao }) {
 
     return (
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-            <div className="bg-white rounded-xl p-8 max-w-md w-full mx-4 shadow-2xl">
-                <div className="flex flex-col items-center justify-center bg-gradient-to-b from-blue-50 to-purple-50 p-8">
+            <div className="bg-gray-800 rounded-xl p-8 max-w-md w-full mx-4 shadow-2xl">
+                <div className="flex flex-col items-center justify-center bg-gradient-to-b from-gray-700 to-gray-500 p-8">
                     <div className="w-full max-w-2xl text-center space-y-8">
-                        <Header title={title} className="text-4xl md:text-5xl font-bold text-red-600 mb-8 animate-bounce" />
+                        <Header title={title} className="text-4xl md:text-5xl font-bold mb-8 animate-bounce" />
                         
-                        <div className="bg-white rounded-lg shadow-lg p-6 ">
+                        <div className="bg-gray-500 rounded-lg shadow-lg p-6 ">
                             <p className="text-xl md:text-2xl text-gray-800 font-medium leading-relaxed">
                                 {message}
                             </p>
                             
-                            <p className="text-lg md:text-xl text-gray-600 italic border-t-2 border-dashed border-gray-200 pt-4">
+                            <p className="text-lg md:text-xl text-zinc-200 italic border-t-2 border-dashed border-gray-200 pt-4">
                                 {messagePontuacao}
                             </p>
                         </div>
